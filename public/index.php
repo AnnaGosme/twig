3aec39d1b6c5b7d0d1ac1515fd6c5e11e3a5bf1b
AnnaGosme <anna.gosme@gmail.com>
2024-05-14 22:16:14 +0200

    same page display

--- public/index.php
+++ public/index.php
@@ -1,7 +1,11 @@
 <?php
 
+require_once '../vendor/autoload.php';
+
 // Get a $twig object from this file.
 require_once __DIR__ . '/../config/twig.php';
+
+require_once __DIR__ . '/products.php';
 
 
 $name = 'Wilder';
